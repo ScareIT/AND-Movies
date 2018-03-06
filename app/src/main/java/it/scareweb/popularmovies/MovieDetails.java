ccaae3fc35b05171517a1b462132197f7e416ad4
Luca Caregnato <luca.caregnato@cgn.it>
2018-03-06 13:13:21 +0100

    Experiments with byte arrays and db persistence.

--- app/src/main/java/it/scareweb/popularmovies/MovieDetails.java
+++ app/src/main/java/it/scareweb/popularmovies/MovieDetails.java
@@ -4,8 +4,11 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +21,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.ByteBuffer;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -44,6 +56,9 @@ public class MovieDetails extends AppCompatActivity {
     @BindView(R.id.details_movie_poster)
     ImageView poster;
 
+    @BindView(R.id.details_movie_poster_clone)
+    ImageView posterClone;
+
     @BindView(R.id.add_to_favourites)
     ImageButton addToFavouritesIcon;
 
@@ -52,6 +67,8 @@ public class MovieDetails extends AppCompatActivity {
     private boolean movieSaved;
 
     Uri movies;
+
+    Movie selectedMovie;
 
     MovieDetails() {
         movieSaved = false;
@@ -64,16 +81,19 @@ public class MovieDetails extends AppCompatActivity {
         ButterKnife.bind(this);
 
         intent = getIntent();
-        Movie selectedMovie = (Movie)intent.getSerializableExtra("MOVIE");
+        selectedMovie = (Movie)intent.getSerializableExtra("MOVIE");
         movieId = selectedMovie.getId();
         bigTitle.setText(selectedMovie.getTitle());
         vote.setText(selectedMovie.getMovieVote());
         plot.setText(selectedMovie.getMoviePlot());
         releaseDate.setText(selectedMovie.getMovieReleaseDate());
         poster.setContentDescription(selectedMovie.getTitle() + " poster");
-        Picasso.with(this)
+//        Picasso.with(this)
+//                .load(SettingsAPI.IMAGE_URL + SettingsAPI.IMAGE_SIZE_NORMAL + selectedMovie.getPicture())
+//                .into(poster);
+                Picasso.with(this)
                 .load(SettingsAPI.IMAGE_URL + SettingsAPI.IMAGE_SIZE_NORMAL + selectedMovie.getPicture())
-                .into(poster);
+                .into(posterClone);
 
         movies = MovieProvider.BASE_CONTENT_URI;
         movies = movies.buildUpon().appendPath(MovieProvider.PATH_FAVOURITES).build();
@@ -117,6 +137,51 @@ public class MovieDetails extends AppCompatActivity {
                 bigTitle.getText().toString());
         values.put(DbManager.MOVIE_VOTE,
                 vote.getText().toString());
+
+        //region Blob experiments
+
+        URLConnection ucon = null;
+        InputStream is = null;
+        try {
+            ucon = new URL(SettingsAPI.IMAGE_URL + SettingsAPI.IMAGE_SIZE_NORMAL + selectedMovie.getPicture()).openConnection();
+            is = ucon.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedInputStream bis = new BufferedInputStream(is);
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        byte[] data = new byte[50];
+        int current = 0;
+
+        try {
+            while((current = bis.read(data,0,data.length)) != -1){
+                buffer.write(data,0,current);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        poster.setImageBitmap(BitmapFactory.decodeByteArray( buffer.toByteArray(),
+                0, buffer.toByteArray().length));
+
+        Bitmap bitmap = ((BitmapDrawable)poster.getDrawable()).getBitmap();
+        //ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        //bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        //byte[] byteArray = stream.toByteArray();
+        int size = bitmap.getRowBytes() * bitmap.getHeight();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(size);
+        bitmap.copyPixelsToBuffer(byteBuffer);
+        byte[] byteArray1 = byteBuffer.array();
+
+        posterClone.setImageBitmap(BitmapFactory.decodeByteArray( byteArray1,
+                0, byteArray1.length));
+
+        values.put(DbManager.MOVIE_POSTER,
+                buffer.toByteArray());
+
+        //endregion
 
         Uri uri = getContentResolver().insert(
                 movies, values);
